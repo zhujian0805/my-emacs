906bee6680d8c66c6dbb126782ad162a5d3cbc5a
James Zhu <jzhu@blizzard.com>
2017-05-04 15:31:22 +0800

    This commit has the following change(s): ------------------------------------------------  M .spacemacs.d/init.el ------------------------------------------------

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -319,6 +319,7 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq dotspacemacs-configuration-layer-path '"~/.spacemacs.d/layers")
   (push "~/.spacemacs.d/customs/" load-path)
   (require 'customs)
   (require 'org-line-break)
