de1390c57e35f9d8b53072a20b007a1c645d7f5c
James Zhu <jzhu@blizzard.com>
2017-05-04 17:15:02 +0800

    This commit has the following change(s): ------------------------------------------------  M .spacemacs.d/customs/you-dao.el ------------------------------------------------

--- .spacemacs.d/customs/you-dao.el
+++ .spacemacs.d/customs/you-dao.el
@@ -1,3 +1,4 @@
-(define-key evil-insert-state-map (kbd "C-y") 'youdao-dictionary-search)
+;; (define-key evil-insert-state-map (kbd "C-y") 'youdao-dictionary-search)
+(global-set-key (kbd "C-y") 'youdao-dictionary-search)
 
 (provide 'you-dao)
